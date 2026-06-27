76201a8398edc8fc8fc829690d821692cc743576
AvniJ237 <avnijain2307@gmail.com>
2026-06-27 14:55:02 +0530

    Implement menu for number checks and calculations
    Added a menu for number classification and calculations.

--- advancenumber.c
+++ advancenumber.c
@@ -1,1 +1,13 @@
-
+//incomplete code cause i am still working on it 
+#include <stdio.h>
+int main(){
+    int choice,a,b;
+    printf("Enter 1 to find if a number is armstrong number\n 2 to find if a number is strong number\n  3 to find if a num  is harshad number \n 4 to find LCM of number\n 5 to find HCF of number");
+    scanf("%d",&choice);
+    switch(choice)
+      {case 1: //check if sum of digits to the power of number of digit is equal to number
+    case 2://check if sum of digits factorial is equal to number
+    case 3: //number divisible by sum of digits(num%sum==0)
+      }
+return 0;
+}
