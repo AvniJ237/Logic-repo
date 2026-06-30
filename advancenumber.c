0e4de99e5b03f72645249fe37711e7a339a66ee2
AvniJ237 <avnijain2307@gmail.com>
2026-06-30 20:03:22 +0530

    added case 2

--- advancenumber.c
+++ advancenumber.c
@@ -1,17 +1,41 @@
 //incomplete code cause i am still working on it 
 #include <stdio.h>
 int main(){
-    int choice,a,b,sum=0;
+    int choice,a,b,sum=0,i,n,product=1;
     printf("Enter 1 to find if a number is armstrong number\n 2 to find if a number is strong number\n  3 to find if a num  is harshad number \n 4 to find LCM of number\n 5 to find HCF of number");
     scanf("%d",&choice);
     switch(choice)
       {case 1: //check if sum of digits to the power of number of digit is equal to number
     case 2://check if sum of digits factorial is equal to number
+       printf("Enter the number");
+       scanf("%d",&a);
+        if(a==0)
+       { printf("zero is not a strong number");}
+        else {
+            b=a;
+       while(a!=0)
+       { n = a % 10;
+
+           product = 1;
+           for(i = 1; i <= n; i++)
+           {
+        product *= i;
+        }
+
+           sum+=product;
+           a=a/10;
+       }
+      if(sum==b)
+         {  printf("strong number ");
+      }
+        else 
+      {printf("not strong number");}}
+       break;
     case 3: //number divisible by sum of digits(num%sum==0)
        printf("Enter the number");
        scanf("%d",&a);
         if(a==0)
-       { printf("zero can't be accepted");}
+       { printf("zero is not a valid input");}
         else {
             b=a;
        while(a!=0)
@@ -23,5 +47,7 @@ int main(){
       }
         else 
       {printf("not harshad number");}}
+      break;
+    case 4: //lcm of two numbers
 return 0;
 }
