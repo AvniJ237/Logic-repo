2685eb01ef9a5afa7da8bfa41443d06be3af652b
AvniJ237 <avnijain2307@gmail.com>
2026-07-09 21:24:51 +0530

    adding multiple words in hangman starting a new game in python

--- hangman.c
+++ hangman.c
@@ -7,23 +7,33 @@ number of lives left
  also add mutliple words for computer to choose from */
  #include<stdio.h>
 #include<string.h> 
+#include<stdlib.h>
+#include<time.h>
  int main()
- {
-    char word[8]="winning",guess[26],letter, string[8]="-------";
-    int lives=5,n=0;
+ {  srand(time(0));
+    char word[][15]={"winning","lantern","cottage","meadow","willow","calico","choclate","marshmallow","leapord","butterfly"},guess[26],letter, string[15];
+    int lives=5,n=0,j=rand()%10;
+    int length = strlen(word[j]);
+for(int i = 0; i < length; i++)
+{string[i] = '-';}
+string[length] = '\0';
     printf("Welcome to Hangman! You have %d lives to guess the word.\n", lives);
-    printf("The word has %lu letters.\n", sizeof(word) );
+    printf("The word has %lu letters.\n", strlen(word[j]));
     printf("You can guess one letter at a time. Good luck!\n");  
     while(lives > 0) {
     printf("Current progress: %s\n", string);
-    printf("Letters guessed so far: ");
+    printf("Wrong letters guessed so far: ");
     for (int i = 0; i < n; i++) {
     printf("%c ", guess[i]);}
         printf("\nEnter a letter: ");
     scanf(" %c", &letter);
+    if (strchr(guess, letter) != NULL || strchr(string, letter) != NULL) {
+        printf("You have already guessed the letter '%c'. Try a different letter.\n", letter);
+        continue;
+    }
     int count = 0;
-    for (int i = 0; i < 7; i++) {
-        if (word[i] == letter) {
+    for (int i = 0; i < strlen(word[j]) ; i++) {
+        if (word[j][i] == letter) {
             string[i] = letter;
             count++;
         }
@@ -35,10 +45,14 @@ number of lives left
         printf("Incorrect guess. You have %d lives left.\n", lives);
         guess[n] = letter;
         n++;
+        guess[n] = '\0';
     }
-    if (strcmp(string, word) == 0) {
-        printf("Congratulations! You've guessed the word: %s\n", word);
+    if (strcmp(string, word[j]) == 0) {
+        printf("Congratulations! You've guessed the word: %s\n", word[j]);
         break;
+    }
+    if (lives == 0) {
+        printf("Game over! You've run out of lives. The word was: %s\n", word[j]);
     }
 }
     return 0;
---
+++ mastermind.py
@@ -0,0 +1,23 @@
+'''create a random 4 digits in a specific order and ask the user to guess it. used '''
+import random
+digits = [0,1,2,3,4,5,6,7,8,9]
+random.shuffle(digits)
+secret = digits[:4]
+while True:
+    guess = [int(d) for d in input("Enter your 4-digit guess: ")]
+    if len(guess) == 4:
+        continue
+    else:
+        print("Invalid input. Please enter exactly 4 digits between 0 and 9.")
+
+    if guess == secret:
+        print("Congratulations! You guessed the number.")
+        break
+    else:
+        for i in range(4):
+            for j in range(4):
+                if guess[i] == secret[j] and i == j:
+                    print("Correct number in correct position:", guess[i])
+                elif guess[i] == secret[j] and i != j:
+                    print("Correct number in wrong position:", guess[i])
+
