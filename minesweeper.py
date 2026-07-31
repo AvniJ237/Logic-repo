dab51934f5320e86f4d95a1ef8cdc149688f16b9
AvniJ237 <avnijain2307@gmail.com>
2026-07-31 10:45:08 +0530

    adding the count feature

--- minesweeper.py
+++ minesweeper.py
@@ -4,27 +4,30 @@ number themselves will be used to count mines in mini 3x3 grid
 try to make board itself an input method
 mines should not be revealed at the start
 give user a flag option and make sure flag doesnt explod the mine
-use ! for flaged """
+use ! for flaged 
+need to solve problem with number that display as refence also need to open a pact before hand"""
 import random
-ground=[['-','A','B','C','D','E','F','G','H'],
-        ['1','_','_','_','_','_','_','_','_'],
-        ['2','_','_','_','_','_','_','_','_'],
-        ['3','_','_','_','_','_','_','_','_'],
-        ['4','_','_','_','_','_','_','_','_'],
-        ['5','_','_','_','_','_','_','_','_'],
-        ['6','_','_','_','_','_','_','_','_'],
-        ['7','_','_','_','_','_','_','_','_'],
-        ['8','_','_','_','_','_','_','_','_']
+ground=[['-','A','B','C','D','E','F','G','H','-'],
+        ['1','_','_','_','_','_','_','_','_','1'],
+        ['2','_','_','_','_','_','_','_','_','2'],
+        ['3','_','_','_','_','_','_','_','_','3'],
+        ['4','_','_','_','_','_','_','_','_','4'],
+        ['5','_','_','_','_','_','_','_','_','5'],
+        ['6','_','_','_','_','_','_','_','_','6'],
+        ['7','_','_','_','_','_','_','_','_','7'],
+        ['8','_','_','_','_','_','_','_','_','8'],
+        ['-','A','B','C','D','E','F','G','H','-']
 ]
-board=[['-','A','B','C','D','E','F','G','H'],
-        ['1','_','_','_','_','_','_','_','_'],
-        ['2','_','_','_','_','_','_','_','_'],
-        ['3','_','_','_','_','_','_','_','_'],
-        ['4','_','_','_','_','_','_','_','_'],
-        ['5','_','_','_','_','_','_','_','_'],
-        ['6','_','_','_','_','_','_','_','_'],
-        ['7','_','_','_','_','_','_','_','_'],
-        ['8','_','_','_','_','_','_','_','_']
+board=[['-','A','B','C','D','E','F','G','H','-'],
+        ['1','_','_','_','_','_','_','_','_','1'],
+        ['2','_','_','_','_','_','_','_','_','2'],
+        ['3','_','_','_','_','_','_','_','_','3'],
+        ['4','_','_','_','_','_','_','_','_','4'],
+        ['5','_','_','_','_','_','_','_','_','5'],
+        ['6','_','_','_','_','_','_','_','_','6'],
+        ['7','_','_','_','_','_','_','_','_','7'],
+        ['8','_','_','_','_','_','_','_','_','8'],
+        ['-','A','B','C','D','E','F','G','H','-']
 ]
 for row in board:
     for cell in row:
@@ -40,9 +43,21 @@ while mines_placed < 8:
         mines_placed += 1
 #add numbers to the ground
 #for row in ground:
-   # for cell in row: 
+# for cell in row: 
 #while all mines not flaged and no mines exploded and places still left to open
-
+for row in range(1,9):
+    for col in range(1,9):
+        if ground[row][col]=='_':
+            count=0
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    if ground[row+i][col+j]=='*':
+                        count+=1
+            ground[row][col]=str(count)  
+for row in ground:
+    for cell in row:
+        print(cell, end=" ")
+    print()
 while True:
     work=input("open or flag(O/F):").upper()
     cell=input("enter the cell number(For eg: Z9)").upper()
@@ -66,3 +81,4 @@ while True:
             print()
     else:
         print("Invalid input") 
+
